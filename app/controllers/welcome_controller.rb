09fdd83d33fbcc51d07116f91cd7af935c9bf448
Katherine McClintic <kdmcclin@gmail.com>
2014-12-15 13:50:18 -0500

    Add custom get prompt route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
 	def index
+	end
+
+	def get_prompts
 		@one_prompt = Prompt.all.sample
-		@two_prompts = Prompt.all.sample(2)
-		@three_prompts = Prompt.all.sample(3)
+		# @two_prompts = Prompt.all.sample(2)
+		# @three_prompts = Prompt.all.sample(3)
 	end
 end
