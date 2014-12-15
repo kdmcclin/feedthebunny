c0752dde7a5c0bbfcb8470c6f4900276478f1757
Katherine McClintic <kdmcclin@gmail.com>
2014-12-15 13:48:41 -0500

    Add custom prompt route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get '/get_prompts' => 'welcome#get_prompts'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
