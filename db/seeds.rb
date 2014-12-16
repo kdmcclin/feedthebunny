b3a9bb64909ded2f28cb1865e14413e0be2ef2e4
Katherine McClintic <kdmcclin@gmail.com>
2014-12-15 20:11:51 -0500

    Add a few starter seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create!([
+  {label: "mythical creatures"},
+  {label: "holidays"},
+  {label: "Chanukah"}
+])
+Prompt.create!([
+  {text: "mermaids"},
+  {text: "I accidentally set the kitchen on fire frying things and you are an attractive firefighter."}
+])
+PromptCategory.create!([
+  {prompt_id: 1, category_id: 1},
+  {prompt_id: 2, category_id: 2},
+  {prompt_id: 2, category_id: 3}
+])
