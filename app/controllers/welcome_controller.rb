3f57403d447347a61e77f8e961c5e1382333e9ca
Katherine McClintic <kdmcclin@gmail.com>
2015-02-01 22:27:08 -0500

    Change random selection

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 	end
 
 	def get_prompts
-		@one_prompt = Prompt.all.sample
+		@one_prompt = Prompt.order("RANDOM()").first
 		# @two_prompts = Prompt.all.sample(2)
 		# @three_prompts = Prompt.all.sample(3)
 	end
