e4cff8c900a03f61e6b59aa498e915e083b5f82d
Katherine McClintic <kdmcclin@gmail.com>
2015-02-02 21:40:35 -0500

    Rewire join table seeding

--- db/seeds/03_prompt_categories.rb
+++ db/seeds/03_prompt_categories.rb
@@ -1,9 +1,4 @@
 PromptCategory.create!([
   {prompt_id: 1, category_id: 1},
-  {prompt_id: 2, category_id: 2},
-  {prompt_id: 2, category_id: 3},
-  {prompt_id: 3, category_id: 2},
-  {prompt_id: 3, category_id: 3},
-  {prompt_id: 4, category_id: 2},
-  {prompt_id: 4, category_id: 4}
+  {prompt_id: 2, category_id: 1},
 ])
