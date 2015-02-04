ac00f52e4dbad14b9dcd1905d49e5bc0c28ece3d
Katherine McClintic <kdmcclin@gmail.com>
2015-02-04 16:42:16 -0500

    Add index and show routes for category

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :categories, only: [:index, :show]
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
   get '/get_prompts' => 'welcome#get_prompts'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
