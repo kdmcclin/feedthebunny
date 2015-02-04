d2fe04365fb14ddc1ef1cfbb8cd3a1f0a80d9487
Katherine McClintic <kdmcclin@gmail.com>
2015-02-04 17:12:52 -0500

    Shuffle categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,8 @@
 class CategoriesController < ApplicationController
 	def index
-		@categories1 = Category.all.where(id: 1..15)
-		@categories2 = Category.all.where(id: 16..30)
-		@categories3 = Category.all.where(id: 31..45)
-		@categories4 = Category.all.where(id: 46..60)
+		@categories1 = Category.all.where(id: 1..15).shuffle
+		@categories2 = Category.all.where(id: 16..30).shuffle
+		@categories3 = Category.all.where(id: 31..45).shuffle
+		@categories4 = Category.all.where(id: 46..60).shuffle
 	end
 end
