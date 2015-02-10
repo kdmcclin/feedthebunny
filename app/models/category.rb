d62df32ac3d1062c22ad07b8e9e538c8f81b223c
Katherine McClintic <kdmcclin@gmail.com>
2015-02-10 15:50:50 -0500

    Correct assocations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
+	has_many :prompt_categories
 	has_many :prompts, through: :prompt_categories
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,3 +1,4 @@
 class Prompt < ActiveRecord::Base
+	has_many :prompt_categories
 	has_many :categories, through: :prompt_categories
 end
