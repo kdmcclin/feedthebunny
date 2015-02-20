7f51f9f6da7a6e714796afabecd42bdd42203ab5
Katherine McClintic <kdmcclin@gmail.com>
2015-02-20 00:08:37 -0500

    Add redirect if html

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
 
 	def get_prompts
 		respond_to do |format|
+			format.html { redirect_to root_path }
 			format.json { render :json => @one_prompt = Prompt.order("RANDOM()").first }
 		end
 
