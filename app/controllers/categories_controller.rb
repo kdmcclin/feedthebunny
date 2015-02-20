30e6a5f94a4326ac1d4d799db2ffa2010e529686
Katherine McClintic <kdmcclin@gmail.com>
2015-02-20 13:04:43 -0500

    Alter category groupings

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,8 @@
 class CategoriesController < ApplicationController
 	def index
-		@categories1 = Category.all.where(id: 1..15).shuffle
-		@categories2 = Category.all.where(id: 16..30).shuffle
-		@categories3 = Category.all.where(id: 31..45).shuffle
-		@categories4 = Category.all.where(id: 46..60).shuffle
+		@categories1 = Category.all.where(id: 1..8)
+		@categories2 = Category.all.where(id: 9..16)
+		@categories3 = Category.all.where(id: 17..24)
+		@categories4 = Category.all.where(id: 25..32)
 	end
 end
