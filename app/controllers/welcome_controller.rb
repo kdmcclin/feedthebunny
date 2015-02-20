dd70fe84f30a422f7436c00c10709e8cefa79dfd
Katherine McClintic <kdmcclin@gmail.com>
2015-02-20 13:07:40 -0500

    Remove unnecessary comments

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,8 +7,5 @@ class WelcomeController < ApplicationController
 			format.html { redirect_to root_path }
 			format.json { render :json => @one_prompt = Prompt.order("RANDOM()").first }
 		end
-
-		# @two_prompts = Prompt.all.sample(2)
-		# @three_prompts = Prompt.all.sample(3)
 	end
 end
