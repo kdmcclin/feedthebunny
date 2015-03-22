a12dc1484655c2b96932575c1022543f02b0234e
Katherine McClintic <kdmcclin@gmail.com>
2015-03-21 21:47:12 -0400

    Update controller to take into account additional category w/id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
 		@categories1 = Category.all.where(id: 1..8)
 		@categories2 = Category.all.where(id: 9..16)
 		@categories3 = Category.all.where(id: 17..24)
-		@categories4 = Category.all.where(id: 25..32)
+		@categories4 = Category.all.where(id: 25..33)
 		
 		@categories = Category.all
 	end
